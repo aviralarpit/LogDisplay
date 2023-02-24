bc5a1230708c29308a2b58f424fe0d34b44a046f
Arpit Kushwaha <119445468+aviralarpit@users.noreply.github.com>
2023-02-24 12:23:20 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,36 @@
+/*
+******************************************************************************
+* License Information: Accord Global Technology Solutions Private Limited.
+* 72 & 73, Krishna Reddy Colony, Domlur layout
+* Bangalore - 560071, INDIA
+* Licensed software and all rights reserved.
+******************************************************************************
+* File:                 main.cpp
+*
+* Description:          QT main class
+*
+* Compiler:
+*
+* Compiler options:
+*
+* Pragma:
+*
+* H/W platform:
+*
+* Portability:
+*
+* Author(s):            Arpit K @ AGTSPL
+*
+* Classes:
+*
+* References:
+*
+* Version History:
+* <Version Number> <Author> <date> <defect Number> <Modification made
+* and reason
+* for modification>
+* 1.1
+*****************************************************************************/
 #include "logdisplay.h"
 #include<QApplication>
 
